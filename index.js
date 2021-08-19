c56bb5a9e8b75592935703a001b807244905c752
Rick <a0928202539@gmail.com>
2021-08-19 22:49:53 +0800

    test test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   console.log("hithere");
-  res.send({ hi: "listen to port 5000" });
+  res.send({ bye: "bye bye~" });
 });
 
 const PORT = process.env.PORT || 5000;
